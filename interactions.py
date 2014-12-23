61ed94eb152631519e9632468e2c7bc7c8a7d1ee
Nymeria8 <joana.fino@gmail.com>
2014-12-23 11:14:52 +0000

    code with coments. template readme

--- interactions.py
+++ interactions.py
@@ -1,7 +1,23 @@
+"""
+Recives:
+-blast output in outfmt-7 against a database of sequences with interactions known,
+-a interaction file (gene1 \t gene2 \t interactiontype \n),
+-variable number of DESEQ output files of our genes
+
+Returns:
+-file with interactions of our sequences, found by homologies:
+	-with the names of the sequences in the database
+	-with the names of our sequences (default)
+-file with genename \t fcvalue1 \t fcvalue2 \t fcvalue3 ....
+
+This files can be imediatly feeded to cytoscape (http://www.cytoscape.org/) for network construction
+
+Usage: python interactions.py blastoutput correspondance_file output_correspondance output_expression DESEQ2_ouput_files[variable]
+"""
 
 from sys import argv
 
-def parse_expression (infile_exp):#recebe ficheiros de expressao
+def parse_expression (infile_exp):#parse the DESEQ2 output files. output: dictionary name - fold change
 	exp=open(infile_exp)
 	dic_exp={}
 	for i in exp:
@@ -11,23 +27,23 @@ def parse_expression (infile_exp):#recebe ficheiros de expressao
 	exp.close()
 	return dic_exp
 
-def read_homology_tab(infile1): #recebe input do blast
+def read_homology_tab(infile1): #parse the tabular file from blast - outfmt=7. returns 1 dictionaries with hit-gene and 2 sets, one with the genes and other with hits
 	tab=open(infile1)
 	homologies={}
 	homologies_rev={}
 	for i in tab:
 		line=i.split("\t")
-		if line[0].startswith("#")==False and line[0] not in homologies and float(line[2])>=90.0 and int(line[3])>=100: # identity > 90%; alignment>100aa
+		if line[0].startswith("#")==False and line[0] not in homologies and float(line[2])>=90.0 and int(line[3])>=100: # cutoff results: identity > 90%; alignment>100aa
 			homologies[line[0]]=line[1]
 			homologies_rev[line[1]]=line[0]	
 	tab.close()
-	return homologies, homologies_rev, set(homologies.values()), set(homologies.keys())
+	return homologies_rev, set(homologies.values()), set(homologies.keys())
 
-homologies, rev, homologies_genes, homologies_vitis=read_homology_tab(argv[1])#recebe o tab do blast
+rev, homologies_genes, homologies_vitis=read_homology_tab(argv[1])#returns the read_homology_tab, as global, so it can be used by several other fuctions
 
 
-def read_protein_actions(infile2):#recebe string iteraction
-	interaction=open(infile2)
+def read_protein_actions(infile2):#parses the interaction file. http://string-db.org/ file or any other file with element one {tab} element two {tab} type of interaction
+	interaction=open(infile2)#returns a dictionary with the interactions of the homologies found
 	pairs={}
 	for i in interaction:
 		line=i.split("\t")
@@ -49,25 +65,25 @@ def read_protein_actions(infile2):#recebe string iteraction
 	return pairs
 
 
-def generate_edges(graph): #recebe o output do read_protein_actions
+def generate_edges(graph): #parses the output of read_protein_actions, and identifies the homologies of our sequences. returns a list of tuples with the interactions of the sequences by the homologies, not the genes
 	edges = []
 	edges_revised=[]
 	for node in graph:
 		for neighbour in graph[node]:
 			if neighbour.endswith("b"):
-				edges.append((node, neighbour[:-1],"binding"))
+				edges.append((node, neighbour[:-1],"binding"))#type of interaction
 			if neighbour.endswith("r"):
-				edges.append((node, neighbour[:-1],"reaction"))
+				edges.append((node, neighbour[:-1],"reaction"))#type of interaction
 			if neighbour.endswith("c"):
-				edges.append((node, neighbour[:-1],"catalysis"))
+				edges.append((node, neighbour[:-1],"catalysis"))#type of interaction
 	for puples in edges:
 		empty=(puples[1], puples[0], puples[2])
 		if empty not in edges_revised and puples not in edges_revised:
 			edges_revised.append(puples)
-	print (edges_revised)
+	print (edges_revised)#because of the different types of interaction, a pair of genes can apear several times but with a different interaction
 	return edges_revised
 	
-def get_real_names(edges):#recebe o output do generate edges 
+def get_real_names(edges):#uses the output of generate_edges. retrives the  same thing of generate_edges, but with the identifiers of our genes
 	l=[]
 	final_list=[]
 	for tuples in edges:
@@ -83,8 +99,8 @@ def get_real_names(edges):#recebe o output do generate edges
 	return final_list
 		
 
-def get_expression(*argv):
-	dic_fc={}
+def get_expression(*argv):#call parse_expression to read the DESEQ output and select the ones correspondant to the used genes. if the gene dont apear in the output of DESEQ, a value of 0 is automaticly gived
+	dic_fc={}#recives as many as needed DESEQ outputs
 	for arg in argv:
 		print(arg)
 		dic=parse_expression(str(arg).strip("[]").strip("'"))#tem de ir para um dicionario, que depois vai ser adicionado ao outroz
@@ -94,13 +110,13 @@ def get_expression(*argv):
 			elif value in dic and value in dic_fc:
 				dic_fc[value].append(dic[value])
 			elif value not in dic and value in dic_fc:
-				dic_fc[value].append("0")
+				dic_fc[value].append("0")#absent from the DESEQ output - value 0
 			elif value not in dic and value not in dic_fc:
-				dic_fc[value]=["0"]
-	return dic_fc
+				dic_fc[value]=["0"]#absent from the DESEQ output - value 0
+	return dic_fc#retrives a dictionary: gene:[value1, value2, value3...]
 
 		
-def write_expression (outfile2, dic):
+def write_expression (outfile2, dic):#recives the dictionary from get expression, and write a file with gene_name {tab} value1 {tab} value2 ....
 	output=open(outfile2, "w")
 	for key, value in dic.items():
 		output.write(key+"\t")
@@ -109,7 +125,7 @@ def write_expression (outfile2, dic):
 		output.write("\n")
 	output.close()
     
-def write_csv(edges, outfile):
+def write_csv(edges, outfile):#recives the get_real_names output (default) or read_protein_actions and write a file with name1 {tab} name2 {tab} interaction {enter}
 	output=open(outfile, "w")
 	empty=()
 	for tuples in edges:
@@ -119,6 +135,6 @@ def write_csv(edges, outfile):
 
 
 
-write_csv(get_real_names(generate_edges(read_protein_actions(argv[2]))), argv[3])
+write_csv(get_real_names(generate_edges(read_protein_actions(argv[2]))), argv[3])# change here to change from the bd names or our sequence names
 
 write_expression(argv[4], get_expression(argv[5:]))	
